075d3fdab0fcdb2fd39b783bd04d5ca1b00e6014
Gregory Thaczuk <thaczuk@gmail.com>
2013-07-08 17:10:26 -0600

    Comments views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,62 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
-  def create
-    @comment = Comment.new(comment_params)
+  def index
+    @comments = Comment.all
+  end
 
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'comment was successfully created' }
-      else
-        format.html { render action: 'new' }
-      end
-    end
+  def show
   end
 
   def new
     @comment = Comment.new
   end
 
+  def edit
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @comment }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to comments_url }
+      format.json { head :no_content }
+    end
+  end
+
   private
-    # Never trust parameters from the scary internet
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:body, :user_id, :post_id)
+      params.require(:comment).permit(:body)
     end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,5 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update]
-# root_path           GET         /                                     posts#index
-# posts_path         GET        /posts(.:format)               posts#index
-#                           POST      /posts(.:format)               posts#create
-# new_post_path   GET        /posts/new(.:format)       posts#new
-# edit_post_path    GET       /posts/:id/edit(.:format)  posts#edit
-# post_path           GET       /posts/:id(.:format)          posts#show
-#                           PATCH   /posts/:id(.:format)          posts#update
-#                           PUT       /posts/:id(.:format)          posts#update
 
   def index
     @posts = Post.all
@@ -32,7 +24,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @comment = Comment.new    # ~1:02 in video
+    @comment = Comments.new    # ~1:02 in video
   end
 
   def update
