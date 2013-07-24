d6fb2118fb6d5c2d4459ad436410c8f38c31d959
Gregory Thaczuk <thaczuk@gmail.com>
2013-07-23 19:36:58 -0600

    Helpers: fix_url, display_datetime

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_datetime(dt)
+    dt.strftime("%A, %B %e, %Y @ %H:%M" )
+  end
+
+  def fix_url(url)
+    url.starts_with?('http://') ? url : "http://#{url}"
+  end
 end
